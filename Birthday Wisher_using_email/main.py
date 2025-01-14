a031a321f7d05126ce33710bf8111c10231adacf
mibrahim05 <mohammedibrahim0503@gmail.com>
2025-01-14 23:38:31 +0530

    Add files via upload

---
+++ Birthday Wisher_using_email/main.py
@@ -0,0 +1,52 @@
+
+
+import pandas as pd
+import smtplib
+import datetime as dt
+import random
+
+
+
+
+
+my_email = "testing.tes0011@gmail.com"
+password = "ggie dnsc pjqt cxsq"
+
+today = dt.datetime.now()
+today_tuple = (today.month,today.day)
+
+data = pd.read_csv("birthdays.csv")
+# HINT 3: Use dictionary comprehension to create a dictionary from birthday.csv that is formated like this:
+# birthdays_dict = {
+#     (birthday_month, birthday_day): data_row
+# }
+#Dictionary comprehension template for pandas DataFrame looks like this:
+birthdays_dict = {(data_row["month"],data_row["day"]): data_row for (index, data_row) in data.iterrows()}
+#e.g. if the birthdays.csv looked like this:
+# name,email,year,month,day
+# Angela,angela@email.com,1995,12,24
+#Then the birthdays_dict should look like this:
+# birthdays_dict = {
+#     (12, 24): Angela,angela@email.com,1995,12,24
+# }
+
+#HINT 4: Then you could compare and see if today's month/day tuple matches one of the keys in birthday_dict like this:
+if today_tuple in birthdays_dict:
+    birthdays_person = birthdays_dict[today_tuple]
+    file_path = f"letter_templates/letter_{random.randint(1,3)}.txt"
+    with open(file_path) as letter_file:
+        content = letter_file.read()
+        content = content.replace("[NAME]",birthdays_person["name"])
+
+
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(my_email,password)
+        connection.sendmail(from_addr=my_email,
+                            to_addrs=birthdays_person["email"],
+                            msg=f"Subject:HAPPY BIRTHDAY!\n\n {content}"
+                            )
+
+
+
+
