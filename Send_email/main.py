85f2a37fe730ad4c01a04a2706ac59dd72a35593
mibrahim05 <mohammedibrahim0503@gmail.com>
2025-01-14 23:36:25 +0530

    Add files via upload

---
+++ Send_email/main.py
@@ -0,0 +1,53 @@
+# import smtplib
+#
+# my_email = "testing.tes0011@gmail.com"
+# password = "ggie dnsc pjqt cxsq"
+#
+#
+# with smtplib.SMTP("smtp.gmail.com") as connection:
+#     connection.starttls()
+#     connection.login(user=my_email,password=password)
+#     connection.sendmail(from_addr=my_email,to_addrs="plantree19@gmail.com",msg="Subject:Hello\n\n This is the body of the email")
+
+
+# import datetime as dt
+#
+# now = dt.datetime.now()
+# year = now.year
+# print(year)
+#
+# dob = dt.datetime(year=2003, month=3 , day=5)
+# print(dob )
+#
+# with open("quotes.txt") as data:
+#
+
+
+import smtplib
+import datetime as dt
+import random
+
+
+my_email = "testing.tes0011@gmail.com"
+password = "ggie dnsc pjqt cxsq"
+
+
+now = dt.datetime.now()
+
+weekday = now.weekday()
+
+if weekday == 1:
+    with open("quotes.txt") as quote_file:
+        all_quotes = quote_file.readlines()
+        quote = random.choice(all_quotes)
+
+        print(quote)
+
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(my_email,password)
+            connection.sendmail(from_addr=my_email,
+                                to_addrs="plantree19@gmail.com",
+                                msg=f"Subject:Monday Motivation \n\n{quote}"
+                                )
+
